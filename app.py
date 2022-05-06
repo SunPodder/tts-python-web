979bac07090be322fb9335c3c17691cf745d9f0e
SunPodder <contact.sunpodder09@gmail.com>
2022-05-06 11:53:23 +0600

    ...

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
-from flask import Flask, redirect, url_for, request, render_template, send_file
-
-import text2speech as tts
+from flask import Flask, request, render_template, send_file
+import io, time
+from gtts import gTTS as tts
+from os import environ
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+  return render_template("index.html")
 
 @app.route("/download", methods=["POST"])
 def download():
-  text = request.form['text']
-  name = tts.convert(text)
-  tts.delete()
-
-  #return redirect(f"/download?name={name}")
-  return send_file(f"audios/{name}", as_attachment=True)
+  text = request.form.get("text")
+  lang = (request.form.get("lang") or "en")
   
-
-app.run(host='0.0.0.0', port=3000)
+  buf = io.BytesIO()
+  audio = tts(text=text, lang=lang)
+  
+  audio.write_to_fp(buf)
+  buf.seek(0)
+  
+  return send_file(
+    buf,
+    as_attachment=True,
+    download_name=f"Sun_tts_{str(time.time()).split('.')[0]}.mp3",
+    mimetype="audio/mp3"
+  )
+  
+  
+app.run(host="0.0.0.0", port=int((environ.get("PORT") or 3000)), debug=False)
